0380f5066fe338cee2916179efa7b21d18589b8f
Anastasiya Kochetkova <lanaxbanana1@yandex.ru>
2024-11-07 14:31:12 +0300

    v1

--- scripts/index.js
+++ scripts/index.js
@@ -6,4 +6,27 @@
 
 // @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+const deleteCard = (cardElement) => {
+    cardElement.remove();
+};
+
+const createCard = ({ name, link }) => {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__image').alt = name;
+    cardElement.querySelector('.card__title').textContent = name;
+
+    
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () => deleteCard(cardElement));
+    return cardElement;
+};
+
+const renderInitialCards = (cards) => {
+    cards.forEach((card) => placesList.append(createCard(card)));
+};
+
+renderInitialCards(initialCards);
